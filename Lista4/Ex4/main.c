41baebb1544ac0d7af537d9a201e3aa7b5cd181d
Berahthraben <nicolasdemantova@hotmail.com>
2019-04-16 20:33:42 -0300

    Update main.c

--- Lista4/Ex4/main.c
+++ Lista4/Ex4/main.c
@@ -4,16 +4,21 @@
 #include "Fila.h"
 int main(void){
   Fila f;
-  Inicializa_Fila(&f, 20, 4);
+  Inicializa_Fila(&f, 5, 4);
   int i;
   srand(time(NULL));
-  for(i=0;i<20;i++){
-    InsereInicio(&f, rand()%10);
+  for(i=0;i<5;i++){
+    InsereInicio(&f, rand()%10+1);
   }
   MostraFila(f);
   int temp;
-  temp = RemoveFim(&f);
+  temp = RemoveInicio(&f);
   printf("Removido nro: %d\n", temp);
+  MostraFila(f);
+  temp = RemoveInicio(&f);
+  printf("Removido nro: %d\n", temp);
+  MostraFila(f);
+  InsereFim(&f, 400);
   MostraFila(f);
   temp = RemoveInicio(&f);
   printf("Removido nro: %d\n", temp);
