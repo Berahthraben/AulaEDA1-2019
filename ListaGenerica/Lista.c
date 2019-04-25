e7792ba3e7f168b819abc02c0da35bf7ebae28d3
Berahthraben <nicolasdemantova@hotmail.com>
2019-04-24 22:06:38 -0300

    Update Lista.c

--- ListaGenerica/Lista.c
+++ ListaGenerica/Lista.c
@@ -14,20 +14,61 @@ int InserirListaInicio(Lista *l, void *elemento);
     return 0;
   }
   temp->info = malloc(sizeof(l->tam_info));
-  if(novo->info==null){
-    free(novo);
+  if(temp->info==null){
+    free(temp);
     return 0;
   }
   memcpy(temp->info, elemento, l->tam_info);
   temp->prox = l->lista;
   l->lista = temp;
-  void *aux=l->lista;
+  return 1;
+}
+
+int InserirListaFim(Lista *l, void *elemento){
+  Elemento *temp = malloc(sizeof(Elemento));
+  if(temp==null){
+    return 0;
+  }
+  temp->info = malloc(sizeof(l->tam_info));
+  if(temp->info==null){
+    free(temp);
+    return 0;
+  }
+  memcpy(temp->info, elemento, l->tam_info);
+  temp->prox = null;
+  Elemento *aux=l->lista;
   while(aux->prox!=null){
     aux = aux->prox;
   }
-    return 1;
+  aux->prox = temp;
+  return 1;
+}
+void *RemoverListaFim(Lista *l, void *info){
+  if(TestaVazia(l)==VAZIA){
+    return VAZIA;
   }
-int RemoverLista(Lista *l);
+  Elemento *aux = l->lista;
+  while(aux->prox!=null){
+    aux = aux->prox;
+  }
+  memcpy(info, aux->info, l->tam_info);
+  free(aux->info);
+  free(aux);
+  return 1;
+}
+
+int RemoverListaInicio(Lista *l, void *info){
+  if(TestaVazia(l)==VAZIA){
+    return VAZIA;
+  }
+  Elemento *aux1 = l->lista;
+  Elemento *aux2 = aux1->prox;
+  memcpy(info, aux1->info, l->tam_info);
+  free(aux1->info);
+  free(aux1);
+  l->lista = aux2;
+  return 1;
+}
 
 int TestaVazia(Lista *l){
   if(l->lista==null){
@@ -36,6 +77,62 @@ int TestaVazia(Lista *l){
     return 0;
   }
 }
-int MostraElemPosi(Lista *l);
+void *RetornaElemPosi(Lista *l, int posi){
+  if(posi>l->qtd_elem){
+    return null;
+  }
+  Elemento *aux = l->lista;
+  int i;
+  for(i=0;i<posi;i++){
+    aux = aux->prox;
+  }
+  return aux->info;
+}
 
-void DesalocaLista(Lista *l);
+int DesalocaLista(Lista *l){
+  int i;
+  for(i=0;i<l->qtd_elem;i++){
+    void *temp;
+    int temp = RemoverListaInicio(l, temp);
+    if(temp==0){
+      printf("ERRO AO DESALOCAR!\n");
+      return 0;
+    }
+  }
+  return 1;
+/*  Elemento *aux1 = l->lista;
+  Elemento *aux2 = l->lista->prox;
+  int i;
+  for(i=0;i<l->qtd_elem;i++){
+    if(i%2==0){
+      free(aux1->info);
+      free(aux1);
+      aux1 = aux2->prox;
+    }else{
+      free(aux2->info);
+      free(aux2);
+      aux2 = aux1->prox;
+    }
+  }
+  free(l);
+  return 1;
+  */
+}
+void MostraLista(Lista *l, void (*mostra)(void *)){
+  if(TestaVazia(l)==VAZIA){
+    printf("Lista Vazia!\n");
+    return;
+  }else{
+    printf("Dados da lista: \n");
+    Elemento *aux = l->lista;
+    while(aux!=null){
+      mostra(aux->info);
+      aux = aux->prox;
+    }
+  }
+}
+/*
+void MostraFloat(void *x){
+  float *p = x;
+  printf(".1f ", *p);
+}*/
